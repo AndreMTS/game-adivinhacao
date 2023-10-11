b839860e350b4df48279691c62dd9ec0dcfc1bed
andre.matos <andre.matos@sisloc.com.br>
2023-10-11 13:33:47 -0300

    refectore do codigo js css

--- app.js
+++ app.js
@@ -1,104 +1,175 @@
-
 document.addEventListener('DOMContentLoaded', function () {
+    // Função para ocultar/exibir elementos
     function ocultarExibirElemento(element) {
         element.style.display = element.style.display === 'none' ? 'block' : 'none';
     }
 
-    const actionDiv = document.querySelector('.actionDiv');
-    const button = document.createElement('button');
-    button.classList = 'btn-iniciar-game';
-    button.type = 'button';
-    button.textContent = 'Iniciar Game';
-    actionDiv.appendChild(button);
+    // Função para criar elementos HTML de forma genérica
+    function createElement(element) {
+        const newElement = document.createElement(element.tag);
 
+        if (element.texto) {
+            newElement.textContent = element.texto;
+        }
+
+        if (element.className) {
+            newElement.className = element.className;
+        }
+
+        if (element.type) {
+            newElement.type = element.type;
+        }
+
+        if (element.min) {
+            newElement.setAttribute('min', element.min);
+        }
+
+        if (element.max) {
+            newElement.setAttribute('max', element.max);
+        }
+
+        return newElement;
+    }
+
+    // Função para criar o botão de início
+    function buttonIniciar() {
+        const buttonInfo = {
+            tag: 'button',
+            texto: 'Iniciar',
+            className: 'btn-iniciar-game'
+        };
+        const Info = {
+            tag: 'h2',
+            texto: 'Vamos jogar?',
+            className: 'info-iniciar-game'
+        };
+        const buttonElement = createElement(buttonInfo);
+        const infoElement = createElement(Info);
+        const actionDiv = document.querySelector('.actionDiv');
+        actionDiv.appendChild(infoElement);
+        actionDiv.appendChild(buttonElement);
+    }
+    buttonIniciar();
+
+    // Evento de clique no botão "Iniciar"
     const iniciar = document.querySelector('.btn-iniciar-game');
-
     iniciar.addEventListener('click', function () {
+        const actionDiv = document.querySelector('.actionDiv');
         ocultarExibirElemento(actionDiv);
         const campoInput = document.querySelector('.campo-input');
-        const inputNumero = document.createElement('input');
-        inputNumero.type = 'number';
-        inputNumero.classList = 'input-numero';
-        inputNumero.placeholder = 'Digite um número';
-        const button = document.createElement('button');
-        button.type = 'button';
-        button.classList = 'btn-enviar-resultado';
-        button.textContent = 'Enviar';
-        campoInput.appendChild(inputNumero);
-        campoInput.appendChild(button);
+        const containerInfo = {
+            tag: 'div',
+            className: 'container-input'
+        };
+        const container_input = createElement(containerInfo);
+        const resultadoInfo = {
+            tag: 'h2',
+            texto: 'Escolha um número de 0 a 10!!'
+        };
+        const resultado = createElement(resultadoInfo);
+        const inputNumeroInfo = {
+            tag: 'input',
+            type: 'number',
+            className: 'input-numero',
+            min: 0,
+            max: 10
+        };
+        const inputNumero = createElement(inputNumeroInfo);
+        const buttonInfo = {
+            tag: 'button',
+            type: 'button',
+            className: 'btn-enviar-resultado',
+            texto: 'Enviar'
+        };
+        const button = createElement(buttonInfo);
+        container_input.appendChild(resultado);
+        container_input.appendChild(inputNumero);
+        container_input.appendChild(button);
+        campoInput.appendChild(container_input);
 
         const getNumero = document.querySelector('.input-numero');
         const btnEnviar = document.querySelector('.btn-enviar-resultado');
-        
+
         btnEnviar.addEventListener('click', function () {
             const valor = getNumero.value;
 
-        
+            let borderContainer = document.querySelector('.container');
             if (valor !== '') {
-                const numeroAleatorio = Math.floor(Math.random() * 10) + 1; // Gera um número aleatório entre 1 e 10
+                const numeroAleatorio = Math.floor(Math.random() * 11); // Gera um número aleatório entre 0 e 10 (inclusivo)
 
-        
-                const valorNumero = parseInt(valor); // Converte o valor do campo em um número inteiro
-        
+                const valorNumero = parseInt(valor);
+
+                if (valorNumero > 10) {
+                    resultado.innerHTML = `Você não entendeu o jogo? <br> Escolha um número de 0 a 10!`;
+                    borderContainer.style.border = '1px solid red';
+                    return;
+                }
+
                 if (numeroAleatorio === valorNumero) {
-                    console.log(`Você acertou! Número aleatório: ${numeroAleatorio}`);
-                    exibirConteiner()
-                    ocultarExibirElemento(campoInput)
-                    ocultarExibirElemento(iniciar)
+                    borderContainer.style.border = '1px solid #72b20c';
+                    exibirConteiner(numeroAleatorio);
+                    ocultarExibirElemento(campoInput);
+                    ocultarExibirElemento(iniciar);
                 } else {
-                    console.log(`Você errou! Número aleatório: ${numeroAleatorio}`);
+                    resultado.innerHTML = `<span style="color:red">Você errou!</span><br>O número era ${numeroAleatorio}<br>Você chutou ${valorNumero}`;
+                    getNumero.value = '';
+                    getNumero.focus();
+                    borderContainer.style.border = '1px solid red';
                 }
             } else {
-                alert('Preencha o campo antes de gerar o número aleatório.');
+                borderContainer.style.border = '1px solid red';
+                resultado.innerText = 'Escolha um número antes de enviar.';
             }
         });
-        
-});
+    });
 
-    function exibirConteiner(){
-        
-           const conteiner = document.querySelector('.container__conteudo')
-        // Crie o elemento div com a classe 'container__informacoes'
-    const containerInformacoes = document.createElement('div');
-    containerInformacoes.classList.add('container__informacoes');
-
-    // Crie o elemento de imagem
-    const img = document.createElement('img');
-    img.src = './img/trophy.png';
-    img.alt = 'ícone de um troféu';
-
-    // Crie o elemento div com a classe 'container__texto'
-    const containerTexto = document.createElement('div');
-    containerTexto.classList.add('container__texto');
-
-    // Crie o elemento h1
-    const h1 = document.createElement('h1');
-    const span = document.createElement('span');
-    span.classList.add('container__texto-azul');
-    span.textContent = 'acertou!';
-    h1.textContent = 'Você ';
-    h1.appendChild(span);
-
-    // Crie o elemento h2
-    const h2 = document.createElement('h2');
-    h2.textContent = 'Você descobriu o número secreto!';
-
-    // Crie o elemento div com a classe 'input-numero'
-
-
-
-
-    // Adicione os elementos à DOM, respeitando a estrutura
-    containerTexto.appendChild(h1);
-    containerTexto.appendChild(h2);
-;
-    containerInformacoes.appendChild(img);
-    containerInformacoes.appendChild(containerTexto);
- 
-
-    // Adicione o elemento containerInformacoes ao body do documento
-    conteiner.appendChild(containerInformacoes);
+    // Função para exibir informações após acertar o número
+    function exibirConteiner(numeroAleatorio) {
+        const conteiner = document.querySelector('.container__conteudo');
+        const containerInfo = {
+            tag: 'div',
+            className: 'container__informacoes'
+        };
+        const containerInformacoes = createElement(containerInfo);
+        const imgInfo = {
+            tag: 'img',
+            src: './img/trophy.png',
+            alt: 'ícone de um troféu'
+        };
+        const img = createElement(imgInfo);
+        const containerTextoInfo = {
+            tag: 'div',
+            className: 'container__texto'
+        };
+        const containerTexto = createElement(containerTextoInfo);
+        const h1Info = {
+            tag: 'h1'
+        };
+        const h1 = createElement(h1Info);
+        const spanInfo = {
+            tag: 'span',
+            className: 'container__texto-azul',
+            texto: 'acertou!'
+        };
+        const span = createElement(spanInfo);
+        h1.textContent = 'Você ';
+        h1.appendChild(span);
+        const h2Info = {
+            tag: 'h2',
+            texto: 'Você descobriu o número secreto!'
+        };
+        const h2 = createElement(h2Info);
+        const h2_winnerInfo = {
+            tag: 'h2',
+            className: 'text_iniciar',
+            texto: `Número era: ${numeroAleatorio}`
+        };
+        const h2_winner = createElement(h2_winnerInfo);
+        containerTexto.appendChild(h1);
+        containerTexto.appendChild(h2);
+        containerTexto.appendChild(h2_winner);
+        containerInformacoes.appendChild(img);
+        containerInformacoes.appendChild(containerTexto);
+        conteiner.appendChild(containerInformacoes);
     }
-
 });
-
